7fa1ce067c2fc860c686cb88731b2acc3418ec96
Jovansonlee Cesar <ivanceras@gmail.com>
2019-12-28 02:59:33 +0800

    Rewrite the whole project to use restq, a new query url parser

---
+++ examples/simple.rs
@@ -0,0 +1,18 @@
+use inquerest::{
+    self,
+    restq::ast::TableLookup,
+};
+
+fn main() {
+    let url = "person?age=lt.42&(student=eq.true|gender=eq.'M')&group_by=sum(age),grade,gender&having=min(age)=gt.42&order_by=age.desc,height.asc&page=20&page_size=100";
+    let query = inquerest::parse_query(url);
+    println!("query: {:#?}", query);
+    println!(
+        "sql query: {}",
+        query.unwrap().into_query(&TableLookup::new()).to_string()
+    );
+
+    let filter = "age=lt.42&(student=eq.true|gender=eq.'M')&group_by=sum(age),grade,gender&having=min(age)=gt.42&order_by=age.desc,height.asc&page=20&page_size=100";
+    let result = inquerest::parse_filter(filter);
+    println!("filter_only: {:#?}", result);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,17 @@
-pub mod parser;
+pub use restq;
+
+use restq::{
+    ast::{
+        Expr,
+        Select,
+    },
+    Error,
+};
+
+pub fn parse_query(input: &str) -> Result<Select, Error> {
+    restq::parse_query(input)
+}
+
+pub fn parse_filter(input: &str) -> Result<Expr, Error> {
+    restq::parse_filter(input)
+}
